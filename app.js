fa54358d2b18e6f8ef585670b242aff67716bf72
Soltan41 <mahmudovsoltan05@gmail.com>
2024-03-13 14:58:06 +0400

    add

--- app.js
+++ app.js
@@ -1,329 +1,122 @@
 
 
-// }
-// cutveyatek(8)
-// // cutveyatek(6)
-
-// let str=''
-
-// function Piramid(r) {
-//     for (let i = 0; i < r; i++) {
-//       str+=i+1
-//     console.log(str);
-//   }
-// }
-
-
-
-// Piramid(10)
-
-
-//  let numbers = [1, 2, 3, 4, 5]
-//  numbers.forEach(
-//     function (a) {
-//     console.log(a);
-        
-//     }
-//  )
-
-// console.log('hello world');
-
-
-
-
-
-// let text = "";
-
-// for (let i = 0; i < 5; i=i+3) {
-//  console.log("bu yazını beş dəfə yaz");
-// }
-
-// const Soltan="soltan"
-
-// for (let i = 0; i < Soltan.length; i++) {
-//   console.log(Soltan[i]);
-  
-// }
-
-
-// const obj={mashin:"kia",model:"optima",il:"ikimbesh",trafick:"true"}
-
-// for (const key in obj) {
-// if (key=="mashin") {
-//   console.log(key);
-//   console.log(obj[key]);
- 
-  
-// }
-// }
-
-// const arr=["bir","iki","uch","dort"]
-//  let selectditem=false;
-
-// arr.forEach(function name(a) {
-
-//   if (a=="dortt") {
-//  selectditem=true
+// Birinci tapsiriq
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <=i; j++) {
+   document.write(i+"")
     
-//   }
-  
-// })
-// console.log(selectditem);
-
-// const arr=["Ilkin","Revan","Yareb","Qedir"]
-
-
-// for (let i = 0; i < arr.length; i++) {
-
- 
-//   if (arr[i]=="Yareb") {
-// continue
-    
-//   }
-//   console.log(arr[i]);
- 
-  
-// }
-
-
-
-// const mehsul={  mehsul: "meyve",
-// mehsulnovu  : "Alma",
-//  qiymeti : 5,
-// rengi : "blue"}
-
-// console.log(mehsul.mehsulnovu);
-
-
-
-// const cedvel=[1, 2, 3, 4]
-//  cedvel.push(5)
-// console.log(cedvel);
-
-
-// function frint() {
-//   const diller={  mehsul: "meyve",
-//   mehsulnovu  : "Alma",
-//    qiymeti : 5,
-//   rengi : "blue"}
-//   console.log(diller);
-// }
-
-// frint()
-
-
-
-
-// function reqem(usta1, usta2) {
-//   const cemi=usta1+usta2
-//   console.log(cemi);
-  
-// }
-// reqem(5, 4)
-
-// function cumle(student1, student2) {
-//   const sentens=student1+" ve "+student2 +"  birlikde imtahandadir"
-
-
-//   console.log(sentens);
-  
-// }
-// cumle( "arif","ali")
-
-// // cumle("akif", "arif")
-
-
-
-
-
-//  const newdiv=document.createElement("div")
-
-
-// newdiv.innerHTML=('hello world')
-
-// document.body.appendChild(newdiv)
-//  const yenidiv=document.createElement("div")
-// yenidiv.id=("some")
-// document.body.appendChild(yenidiv)
-
-
-// const yenidiv=document.createElement("div")
-
-// yenidiv.innerHTML=('hello world')
-
-// document.body.appendChild(yenidiv)
-// const newdiv=document.createElement("div")
-// newdiv.id=("some")
-// document.body.appendChild( newdiv)   
-
-
-// let button=document.querySelector("button")
-// button.addEventListener("click",function () {
-//     alert("ciliket")
-    
-// })
-
-// function myFunction(p1, p2) {
-//     return p1 * p2;
-//   }
-
-
-
-//   const form=document.querySelector('form')
-
-//   console.log(form);
-
-
-
-
-//     const input=form.querySelector("input")
-
-
-//     console.log(input);
-
-//     const element=input.closest("div")
-
-//     console.log(element);
-
-
-//     const kitab={müəllif: "kiosaki",
-//      kitab:"varli ata kasib ata ", 
-//      ders:"edebiyyat", sinif:"onbir"}
-
-// delete kitab.müəllif  kitab.pop()
-//     kitab.muellif='hill'
-
-
-
-//     delete kitab.pop()
-//     console.log(kitab);
-
-
-
-
-
-
-
-//     const person={
-//         ad:"Soltan",
-//         Soyad:"Mahmudov",
-//         Yash:"26",
-//  fufllname: function () {
-//     return this.Soyad +" "+ this.ad;        
-    
-//  }
-//     }
-//     console.log(person.fufllname);
-
-// const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// fruits.pop();
-// console.log(fruits);
-
-// const laminat=["krem","venge","qara","ag mat","yashil","mavi","red"]
-
-// laminat.push("sari")
-// const yenilaminat=laminat.slice(1,5)
-// console.log(yenilaminat);
-// function validateForm() {
-//     let x = document.forms["name"]["fname"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//   }
-//   validateForm()
-
-// const name = document.getElementById('name')
-// const password = document.getElementById('password')
-// const form = document.getElementById('form')
-// const errorElement = document.getElementById('error')
-
-//  form.addEventListener('submit', (e) => {
-//   let messages = []
-//   if (name.value === ''|| name.value == null){
-//     messages.push( 'Name is required')
-//   }
-
-//   if (password.value.length <= 6){
-//     messages.push('passowerd must be longer than 6 xarekter')
-//    }
-//   if(messages.length > 0){
-//     e.preventDefault()
-//     errorElement.innerText = messages.join(',')
-//   }
-
-
-//  })
-
-function cutveyatek(a) {
-  if (a%2==0) {
-       console.log('cutreqemdi');
   }
-  else{
-      console.log('tekreqemdi');
-  }
-
-
+ document.write("<br>")
 }
 
-cutveyatek(6)
-
-// function polindrom(word) {
-//   const reverseWord=word.split('').reverse().join()
-// return reverseWord
-// }
-
-
-// const result=polindrom('soltan')
-// console.log(result);
 
 
 
-// function polindrom(word) {
-//   const reverseWord=word.split(' ').reverse().join()
-//  if(word===reverseWord){
-//   return true
-//  }
-//  else{
-//   return false
-//  }
-// }
+// ikinci tapsiriq
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <=i; j++) {
+   document.write(j+"")
+    
+  }
+ document.write("<br>")
+}
 
 
-// const result=polindrom('amma')
-// console.log(result);
+// ucuncu tapsiriq
+let count=1
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <=i; j++) {
+   document.write(count+"")
+    count++
+  }
+ document.write("<br>")
+}
+
+// dorduncu tapsiriq
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 4; j >=i; j--) {
+   document.write("&nbsp;");
+    
+  }
+  for (let k = 1; k <=i; k++) {
+    document.write(i + " ");
+     
+   }
+ document.write("<br>");
+}
 
 
 
-// function polindrom(word) {
-//   const reverseWord=word.split("").reverse().join("")
-//  return word===reverseWord ? true : false
+
+// Besinci tapsiriq
+
+let cumle ="Novruz Bayraminiz Mubarek"
+
+let cumle2=cumle.indexOf("B")
+console.log(cumle2);
  
-// }
+// Bunuda funksyada yazdim
+
+function sentencIndexOf(sentence) {
+  return sentence.indexOf("m")
+}
+
+let text = "BU bir cumledir"
+let text2=text
+console.log(sentencIndexOf(text2));
 
 
-// const result=polindrom("amma")
-// console.log(result);
+// Altinci tapsiriq
 
-
-// function name(params) {
-  
-// }
-
-
-// function myCalculator(num1, num2) {
-//   let sum = num1 + num2;
-//   return sum;
-// }
-
-// let result = myCalculator(5, 5);
-// myDisplayer(result);
-
-
-function eks(soz) {
-  const reverseWord=soz.split('').reverse().join('')
-  return reverseWord
+function sentenceLength(metn) {
+  return metn.length;
   
 }
-const result=eks("salam")
-console.log(result);
+
+let cumle4="BU kitab maraqlidir"
+
+let cumle5=cumle4
+
+console.log(sentenceLength(cumle5));
+
+
+
+//Yeddinci tapsiriq
+const i='Bu melumatdi'
+const k ='Bu melumatdir'
+
+if (i===k) {
+  console.log("true");
+  
+}
+
+else{
+  console.log("false");
+}
+
+
+function printPattern(rows) {
+  for (let i = 1; i <= rows; i++) {
+      let row = '';
+      for (let j = 1; j <= i; j++) {
+          row += i;
+      }
+      console.log(row);
+  }
+}
+
+printPattern(4);
+
+
+
+
+for (let i = 1; i <= 4; i++) {
+  let result=""
+  for (let j = 1; j <=i; j++) {
+  result+=j
+    
+  }
+ console.log(result)
+  }
